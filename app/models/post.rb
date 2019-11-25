2c7be05be981e40330782d94d4f2d45d1ea4ded3
AkariYamaguchi <akr03@gmail.com>
2019-11-25 20:17:10 +0900

    upload image

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     # mount_uploader :image, ImageUploader
     # devise :database_authenticatable, :registerable,
     #         :recoverable, :rememberable, :validatable
+    #active storage
+    # has_one_attached :image
 end
