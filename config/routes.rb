0d4f41a758e5498009432e5fe93c44ea495e51c5
AkariYamaguchi <akr03@gmail.com>
2019-12-24 11:39:27 +0900

    fixed design

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "users/:id/edit" =>"users#edit"
   post "users/:id/update" =>"users#edit"
 
+  #投稿
   resources :comments
   get "comments" => "comments#index"
   get "comments/:id" => "comments#show"
@@ -12,15 +13,9 @@ Rails.application.routes.draw do
   get "comments/:id" => "comments#edit"
   delete "commnets/:id" => "comments#destroy"
   post "comments/:id" => "comments#update"
-  #投稿
 
-  get "posts/index" => "posts#index"
-  get "posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
-  get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/update" =>"posts#update"
-  post "posts/:id/destroy" =>"posts#destroy"
-  get "posts/:id" => "posts#show"
+
+
   get "/" => "home#top"
 
   #プロフィール
@@ -29,12 +24,5 @@ Rails.application.routes.draw do
   get "me/edit" => "me#edit"
   patch "me/update" => "me#update"
 
-#   devise_for :mes, controllers: {
-#   confirmations: 'mes/confirmations',
-#   passwords:     'mes/passwords',
-#   registrations: 'mes/registrations',
-#   sessions:      'mes/sessions',
 
-# }
-# get "me/edit" => "mes/registrations#edit"
 end
