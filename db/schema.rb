db5e129d50d6782fae9624d9eeb56cbf3a63b223
AkariYamaguchi <akr03@gmail.com>
2019-12-26 12:35:23 +0900

    newest one

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_081657) do
+ActiveRecord::Schema.define(version: 2019_11_13_045703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,12 +42,10 @@ ActiveRecord::Schema.define(version: 2019_11_14_081657) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "mes", force: :cascade do |t|
+  create_table "me", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
-
   end
 
   create_table "posts", force: :cascade do |t|
